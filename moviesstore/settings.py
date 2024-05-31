bf82c47d6578fd3686b76d69872b1af67e8ecdfc
Daniel Hirata <danielhirata@Daniels-MacBook-Pro.local>
2024-05-31 17:09:49 -0300

    change db

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -80,14 +80,10 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moviesstore',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
